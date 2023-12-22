839b5ad841e60aad3a9ae4cac1b3cf7e88a8c3c1
손유정 <po042@naver.com>
2023-12-22 10:51:34 +0900

    20231222 TIL codingtest

---
+++ codingtest/백준 9095번 1, 2, 3 더하기.py
@@ -0,0 +1,13 @@
+dp = [0] * 12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 12):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
