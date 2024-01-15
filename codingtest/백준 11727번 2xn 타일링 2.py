6247686fa6cd8dba11f26e4c24efb2e10309be2a
손유정 <po042@naver.com>
2024-01-15 15:21:56 +0900

    20240115 TIL codingtest

---
+++ codingtest/백준 11727번 2xn 타일링 2.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(3)
+else:
+    dp = [0, 1, 3] + [0] * (n - 2)
+    for i in range(3, n + 1):
+        dp[i] = dp[i-2] * 2 + dp[i-1]
+    print(dp[n] % 10007)
