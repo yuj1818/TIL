67b4aedc600fc9d29902e5f73377756e70ad37c1
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2024-04-12 00:41:41 +0900

    20240412 TIL codintest

--- codingtest/백준 15638번 감시.py
+++ codingtest/백준 15638번 감시.py
@@ -1,58 +1,46 @@
-import copy
-n, m = map(int, input().split())
+from itertools import chain
+
+N, M = map(int, input().split())
+board = []
 cctv = []
-graph = []
-mode = [
-    [],
-    [[0], [1], [2], [3]],
-    [[0, 2], [1, 3]],
-    [[0, 1], [1, 2], [2, 3], [0, 3]],
-    [[0, 1, 2], [0, 1, 3], [1, 2, 3], [0, 2, 3]],
-    [[0, 1, 2, 3]],
-]
+d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+mode = {
+    1: [[0], [1], [2], [3]],
+    2: [[0, 2], [1, 3]],
+    3: [[0, 1], [1, 2], [2, 3], [0, 3]],
+    4: [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]],
+    5: [[0, 1, 2, 3]]
+}
+min_v = N * M
 
-# 북 - 동 - 남 - 서
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+for i in range(N):
+    line = list(map(int, input().split()))
+    for j in range(M):
+        if line[j] in [1, 2, 3, 4, 5]:
+            cctv.append((line[j], i, j))
+    board.append(line)
 
-for i in range(n):
-    data = list(map(int, input().split()))
-    graph.append(data)
-    for j in range(m):
-        if data[j] in [1, 2, 3, 4, 5]:
-            cctv.append([data[j], i, j])
-
-def fill(board, mm, x, y):
-    for i in mm:
-        nx = x
-        ny = y
-        while True:
-            nx += dx[i]
-            ny += dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                break
-            if board[nx][ny] == 6:
-                break
-            elif board[nx][ny] == 0:
-                board[nx][ny] = 7
-
-def dfs(depth, arr):
-    global min_value
-
+def dfs(depth, board):
+    global min_v
     if depth == len(cctv):
-        count = 0
-        for i in range(n):
-            count += arr[i].count(0)
-        min_value = min(min_value, count)
+        min_v = min(list(chain(*board)).count(0), min_v)
         return
-    temp = copy.deepcopy(arr)
-    cctv_num, x, y = cctv[depth]
-    for i in mode[cctv_num]:
-        fill(temp, i, x, y)
-        dfs(depth+1, temp)
-        temp = copy.deepcopy(arr)
 
+    kind, y, x = cctv[depth]
+    temp = [row[:] for row in board]
+    for dirs in mode[kind]:
+        for direction in dirs:
+            nx = x
+            ny = y
+            while True:
+                nx += d[direction][1]
+                ny += d[direction][0]
+                if 0 <= nx < M and 0 <= ny < N and temp[ny][nx] != 6:
+                    if temp[ny][nx] == 0:
+                        temp[ny][nx] = -1
+                else: break
+        dfs(depth + 1, temp)
+        temp = [row[:] for row in board]
 
-min_value = int(1e9)
-dfs(0, graph)
-print(min_value)
+dfs(0, board)
+print(min_v)
