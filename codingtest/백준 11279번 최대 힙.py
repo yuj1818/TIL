d45ef2a9d190073b4befd11a27832cf0f39f83bc
손유정 <po042@naver.com>
2024-05-27 12:54:45 +0900

    20240527 TIL codingtest

---
+++ codingtest/백준 11279번 최대 힙.py
@@ -0,0 +1,14 @@
+import os, io
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+import heapq
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        print(-heapq.heappop(arr) if arr else 0)
+    else:
+        heapq.heappush(arr, -x)
