3675dd4d811ed046fc2a681212655fc6851e2c11
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-06 14:14:04 +0900

    20250206 TIL codingtest BOJ 1026

---
+++ codingtest/백준 1026번 보물.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+a.sort()
+b.sort(reverse=True)
+print(sum([a[i] * b[i] for i in range(n)]))
