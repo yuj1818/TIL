75e4f57018148a7a56c95c52bc7b8312d46cda39
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-21 11:27:25 +0900

    20250221 TIL codingtest BOJ 3190

--- codingtest/백준 3190번 뱀.py
+++ codingtest/백준 3190번 뱀.py
@@ -1,39 +1,38 @@
-N = int(input())
-K = int(input())
-apples = [list(map(int, input().split())) for _ in range(K)]
-board = [[0 for _ in range(N)] for _ in range(N)]
-board[0][0] = 1
-L = int(input())
-play_time = 0
-x = 0
-y = 0
-dx = 1
-dy = 0
-cnt = 0
-for _ in range(L):
-    s, r = input().split()
-    for _ in range(int(s)):
-        nx = x + 1 * dx
-        ny = y + 1 * dy
-        if 0 <= nx < N and 0 <= ny < N and board[ny][nx] == 0:
-            for ax, ay in apples:
-                if nx == ax and ny == ay:
-                    break
+input = open(0).readline
+dyx = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+trans = lambda x: (int(x[0]), x[1])
+
+def play(board, info, n):
+    ct, i, d = 0, 0, 0
+    board[0][0] = 1
+    s = [(0, 0)]
+    while True:
+        t, r = info[i]
+        dy, dx = dyx[d]
+        diff = t - ct
+        for x in range(diff):
+            ct += 1
+            sy, sx = s[-1]
+            ny, nx = sy + dy, sx + dx
+            if 0 <= ny < n and 0 <= nx < n and board[ny][nx] != 1:
+                if board[ny][nx] == 0:
+                    ey, ex = s.pop(0)
+                    board[ey][ex] = 0
+                board[ny][nx] = 1
+                s.append((ny, nx))
             else:
-                board[y][x] = 0
-            x = nx
-            y = ny
-            board[y][x] = 1
-            cnt += 1
-        else:
-            break
+                return ct
+        d = ((d - 1) if r == 'L' else (d + 1)) % 4
+        i += 1
 
-    if dy == 0 and r == 'L':
-        dx, dy = dy, -1 * dx
-    elif dx == 0 and r == 'D':
-        dx, dy = -1 * dy, dx
-    else:
-        dx, dy = dy, dx
+def main():
+    n = int(input())
+    board = [[0] * n for _ in range(n)]
+    for _ in range(int(input())):
+        y, x = map(int, input().split())
+        board[y - 1][x - 1] = 2
+    info = [trans(input().strip().split()) for _ in range(int(input()))]
+    info.append((info[-1][0] + n + 1, ''))
+    print(play(board, info, n))
 
-print(cnt)
-
+main()
