28d48fbe53aa6acfe2b8355d1eaf474d54a21745
sonyujeong <sonyujeong18@amsquare.ai>
2025-08-01 14:59:38 +0900

    20250801 TIL codingtest BOJ 10093

--- codingtest/백준 10093번 숫자.py
+++ codingtest/백준 10093번 숫자.py
@@ -1,3 +1,6 @@
-a, b = map(int, input().split())
-print(b - a - 1)
-for i in range(a + 1, b): print(i, end=' ')
+a, b = sorted(map(int, input().split()))
+cnt = b - a - 1
+if cnt > 0:
+    print(cnt)
+    for i in range(a + 1, b): print(i, end=' ')
+else: print(0)
