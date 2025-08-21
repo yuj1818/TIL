27e8705b7fa321a5dbdc890eb2cc6f4aeac91efb
sonyujeong <sonyujeong18@amsquare.ai>
2025-08-21 14:54:34 +0900

    20250821 TIL nextJS data cahing

---
+++ web/next/next_data_caching/app/messages/page.js
@@ -0,0 +1,20 @@
+import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
+
+// export const revalidate = 5;
+// export const dynamic = 'force-dynamic';
+
+export default async function MessagesPage() {
+  // const response = await fetch('http://localhost:8080/messages', {
+  //   next: { tags: ['msg'] },
+  // });
+  // const messages = await response.json();
+
+  const messages = await getMessages();
+
+  if (!messages || messages.length === 0) {
+    return <p>No messages found</p>;
+  }
+
+  return <Messages messages={messages} />;
+}
