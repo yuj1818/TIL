df66a84371515f5cd19dfb4c655f2a8b6e4b1d83
sonyujeong <sonyujeong18@amsquare.ai>
2025-08-26 15:45:42 +0900

    20250826 TIL nextJS React Context

--- web/next/next_page_file_based_routing/components/input/comments.js
+++ web/next/next_page_file_based_routing/components/input/comments.js
@@ -1,21 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '@/store/notification-context';
 
 function Comments(props) {
   const { eventId } = props;
+  const notificationCtx = useContext(NotificationContext);
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
 
   useEffect(() => {
     if (showComments) {
+      setIsFetchingComments(true);
       fetch(`/api/comments/${eventId}`)
         .then((res) => res.json())
         .then((data) => {
           setComments(data.comments);
+          setIsFetchingComments(false);
         });
     }
   }, [showComments]);
@@ -25,6 +30,12 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: 'Sending comment...',
+      message: 'Your comment is currently being stored into a database.',
+      status: 'pending',
+    });
+
     fetch(`/api/comments/${eventId}`, {
       method: 'POST',
       body: JSON.stringify(commentData),
@@ -32,8 +43,31 @@ function Comments(props) {
         'Content-Type': 'application/json',
       },
     })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        return res.json().then((data) => {
+          throw new Error(data.message || 'Something went wrong!');
+        });
+      })
+      .then((data) => {
+        setComments((prev) => [...prev, data.comment]);
+
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Your comment was saved!',
+          status: 'success',
+        });
+      })
+      .catch((err) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: err.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
   }
 
   return (
@@ -42,7 +76,12 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments &&
+        (!isFetchingComments ? (
+          <CommentList items={comments} />
+        ) : (
+          <p>Loading...</p>
+        ))}
     </section>
   );
 }
--- web/next/next_page_file_based_routing/components/input/newsletter-registration.js
+++ web/next/next_page_file_based_routing/components/input/newsletter-registration.js
@@ -1,13 +1,21 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '@/store/notification-context';
 
 function NewsletterRegistration() {
   const eamilInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
 
     const enteredEmail = eamilInputRef.current.value;
+
+    notificationCtx.showNotification({
+      title: 'Signing up.',
+      message: 'Registering for newsletter.',
+      status: 'pending',
+    });
 
     fetch('/api/newsletter', {
       method: 'POST',
@@ -16,8 +24,29 @@ function NewsletterRegistration() {
         'Content-Type': 'application/json',
       },
     })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        return res.json().then((data) => {
+          throw new Error(data.message || 'Something went wrong!');
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Succesfully registered for newsletter!',
+          status: 'success',
+        });
+      })
+      .catch((err) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: err.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
   }
 
   return (
