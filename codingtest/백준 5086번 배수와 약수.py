2997f8583287d658ece9394e20d046fb827fab8a
sonyujeong <sonyujeong18@amsquare.ai>
2025-09-12 17:31:09 +0900

    20250912 TIL codingtest BOJ 5086

---
+++ codingtest/백준 5086번 배수와 약수.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+while 1:
+    a, b = map(int, input().split())
+    if a + b == 0: break
+    if (a % b == 0): print('multiple')
+    elif (b % a == 0): print('factor')
+    else: print('neither')
