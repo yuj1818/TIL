e43681dd2316f8d3e5a6bddf8301188fc3de7038
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2025-10-01 20:15:09 +0900

    20251001 TIL codingtest BOJ 13565

--- codingtest/백준 13565번 침투.py
+++ codingtest/백준 13565번 침투.py
@@ -1,12 +1,14 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 input = sys.stdin.readline
-d = [(1, 0), (1, 0), (-1, 0), (-1, 0)]
+d = [(1, 0), (0, 1), (0, -1), (-1, 0)]
 
 def dfs(y, x):
     global sig
-    if y == n - 1: sig = True
     if sig: return
+    if y == m - 1:
+        sig = True
+        return
     for dy, dx in d:
         ny, nx = y + dy, x + dx
         if 0 <= ny < m and 0 <= nx < n and not visited[ny][nx] and not board[ny][nx]:
@@ -19,10 +21,8 @@ board = [list(map(int, input().strip())) for _ in range(m)]
 visited = [[0] * n for _ in range(m)]
 sig = False
 for j in range(n):
-    for i in range(m):
-        if not visited[i][j]:
-            visited[i][j] = 1
-            dfs(i, j)
-        if sig: break
+    if not board[0][j] and not visited[0][j]:
+        visited[0][j] = 1
+        dfs(0, j)
     if sig: break
 print('YES' if sig else 'NO')
