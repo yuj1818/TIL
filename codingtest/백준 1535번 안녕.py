dbf10849cd68a578b6fb44291ea5ad64db1bad73
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2026-03-24 17:16:46 +0900

    20260324 TIL codingtest BOJ 1535

---
+++ codingtest/백준 1535번 안녕.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+L = [0] + list(map(int, input().split()))
+J = [0] + list(map(int, input().split()))
+dp = [[0] * 101 for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, 101):
+        if L[i] <= j: dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - L[i]] + J[i])
+        else: dp[i][j] = dp[i - 1][j]
+print(dp[-1][-2])
