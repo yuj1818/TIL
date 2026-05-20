fa23ddadcd29f4b41fa7123bfc0896b603f8d527
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2026-05-20 13:16:22 +0900

    [Passed] 안전 지대 (156ms, 20MB)

--- codingtest/codetree/trail4/안전 지대/comfort-zone.py
+++ codingtest/codetree/trail4/안전 지대/comfort-zone.py
@@ -1,29 +1,30 @@
 import sys
-sys.setrecursionlimit(2500)
 input = sys.stdin.readline
 
-def dfs(y, x):
-    for dy, dx in d:
-        ny, nx = y + dy, x + dx
-        if not (0 <= ny < n and 0 <= nx < m) or grid[ny][nx] <= k or visited[ny][nx]: continue
-        visited[ny][nx] = 1
-        dfs(ny, nx)
+def bfs(i, j):
+    q = [(i, j)]
+    visited[i][j] = 1
+    while q:
+        y, x = q.pop(0)
+        for dy, dx in d:
+            ny, nx = y + dy, x + dx
+            if not (0 <= ny < n and 0 <= nx < m) or grid[ny][nx] <= k or visited[ny][nx]: continue
+            visited[ny][nx] = 1
+            q.append((ny, nx))
 
 d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-s = sorted(set(x for row in grid for x in row))
-if s[0] - 1 > 0: s = [s[0] - 1] + s
-ak, ac = 0, -1
-for k in s:
-    cnt = 0
+mk = max(map(max, grid))
+ak, ac = mk, 0
+for k in range(1, mk):
     visited = [[0] * m for _ in range(n)]
+    cnt = 0
     for i in range(n):
         for j in range(m):
             if grid[i][j] > k and not visited[i][j]:
-                visited[i][j] = 1
                 cnt += 1
-                dfs(i, j)
-    if cnt > ac:
+                bfs(i, j)
+    if ac < cnt:
         ak, ac = k, cnt
 print(ak, ac)
