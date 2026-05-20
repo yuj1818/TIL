b4edcf863181297942d3d50bee50e062073a48d3
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2026-05-20 11:19:21 +0900

    [Passed] 안전 지대 (1151ms, 132MB)

---
+++ codingtest/codetree/trail4/안전 지대/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]안전 지대](https://www.codetree.ai/trails/complete/curated-cards/challenge-comfort-zone)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 4 / DFS / DFS](https://www.codetree.ai/trail-info/intermediate-low/) |
+| 난이도 | 보통 |
+| 경험치 | 70 XP |
---
+++ codingtest/codetree/trail4/안전 지대/comfort-zone.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(y, x):
+    for dy, dx in d:
+        ny, nx = y + dy, x + dx
+        if not (0 <= ny < n and 0 <= nx < m) or grid[ny][nx] <= k or visited[ny][nx]: continue
+        visited[ny][nx] = 1
+        dfs(ny, nx)
+
+d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+rk, rc = 1, 0
+k, sig = 1, 1
+while sig:
+    sig, cnt = 0, 0
+    visited = [[0] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] > k and not visited[i][j]:
+                sig = 1
+                cnt += 1
+                visited[i][j] = 1
+                dfs(i, j)
+    if cnt > rc:
+        rk, rc = k, cnt
+    k += 1
+print(rk, rc)
