4744335e677510e6d8c96784dc9d191c2559374f
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2026-06-22 14:23:52 +0900

    [Passed] 정수 사각형 차이의 최소 2 (253ms, 23MB)

--- codingtest/codetree/trail4/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ codingtest/codetree/trail4/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,14 +1,29 @@
+MAX = float('inf')
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-dp = [[[float('inf')] * 101 for _ in range(n)] for _ in range(n)]
-dp[0][0][grid[0][0]] = grid[0][0]
-for i in range(n):
-    for j in range(n):
-        for k in range(1, 101):
-            min_k = min(k, grid[i][j])
-            dp[i][j][min_k] = min(dp[i][j][min_k], max(grid[i][j], min(dp[i - 1][j][k], dp[i][j - 1][k])))
-ans = float('inf')
-for k in range(1, 101):
-    if dp[-1][-1][k] != float('inf'):
-        ans = min(ans, dp[-1][-1][k] - k)
+ans = MAX
+dp = [[MAX] * n for _ in range(n)]
+
+def init_dp():
+    for i in range(n):
+        for j in range(n): dp[i][j] = MAX
+    dp[0][0] = grid[0][0]
+    for x in range(1, n):
+        dp[x][0] = max(dp[x - 1][0], grid[x][0])
+        dp[0][x] = max(dp[0][x - 1], grid[0][x])
+
+def find_max_v(min_v):
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] < min_v: grid[i][j] = MAX
+    init_dp()
+    for i in range(1, n):
+        for j in range(1, n):
+            dp[i][j] = max(grid[i][j], min(dp[i - 1][j], dp[i][j - 1]))
+    return dp[-1][-1]
+
+for min_v in range(1, 101):
+    max_v = find_max_v(min_v)
+    if max_v == MAX: continue
+    ans = min(ans, max_v - min_v)
 print(ans)
