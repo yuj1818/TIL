3f4578a1f6f4d244e90990b8bddcc04f45791323
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2026-07-03 22:38:21 +0900

    [Passed] 경험치를 빠르게 얻기 (72ms, 18MB)

---
+++ codingtest/codetree/trail4/경험치를 빠르게 얻기/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]경험치를 빠르게 얻기](https://www.codetree.ai/trails/complete/curated-cards/challenge-gain-exp-quickly)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 4 / DP I / 아이템을 적절히 고르는 문제](https://www.codetree.ai/trail-info/intermediate-low/) |
+| 난이도 | 어려움 |
+| 경험치 | 90 XP |
---
+++ codingtest/codetree/trail4/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -0,0 +1,16 @@
+MIN = -float('inf')
+n, m = map(int, input().split())
+quests = [tuple(map(int, input().split())) for _ in range(n)]
+mv = 0
+for e, t in quests: mv += t
+dp = [[MIN] * (mv + 1) for _ in range(n + 1)]
+dp[0][0] = 0
+for i, (e, t) in enumerate(quests):
+    for j in range(mv + 1):
+        if j >= t: dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - t] + e)
+        dp[i + 1][j] = max(dp[i + 1][j], dp[i][j])
+ans = float('inf')
+for i in range(mv + 1):
+    if dp[n][i] >= m: ans = min(ans, i)
+if ans == float('inf'): ans = -1
+print(ans)
