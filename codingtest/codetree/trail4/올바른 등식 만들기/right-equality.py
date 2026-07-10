6b7e5f088c7198d6719125184dc37ca44c44ca05
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2026-07-10 15:06:57 +0900

    [Passed] 올바른 등식 만들기 (60ms, 17MB)

--- codingtest/codetree/trail4/올바른 등식 만들기/right-equality.py
+++ codingtest/codetree/trail4/올바른 등식 만들기/right-equality.py
@@ -1,13 +1,14 @@
-MIN = -float('inf')
 n, m = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
-dp = [[0] * 41 for _ in range(n + 1)]
-dp[0][20] = 1
-m += 20
+total = sum(arr)
+LIM = min(total * 2, 40)
+dp = [[0] * (LIM + 1) for _ in range(n + 1)]
+dp[0][LIM // 2] = 1
+m += LIM // 2
 for i in range(1, n + 1):
-    for j in range(41):
+    for j in range(LIM + 1):
         if j >= arr[i]:
             dp[i][j] += dp[i - 1][j - arr[i]]
-        if j + arr[i] < 41:
+        if j + arr[i] <= LIM:
             dp[i][j] += dp[i - 1][j + arr[i]]
 print(dp[n][m])
