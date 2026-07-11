d3c66216b441a4f405523f2250f491c4a5df31de
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2026-07-11 22:49:57 +0900

    [Passed] 게임 스튜디오에서 살아남기 (76ms, 18MB)

--- codingtest/codetree/trail4/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
+++ codingtest/codetree/trail4/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -1,10 +1,10 @@
 MOD = 10 ** 9 + 7
 n = int(input())
 dp = [[[0] * 4 for _ in range(4)] for _ in range(n + 1)]
-dp[0][0][0] = 1
-dp[0][1][0] = 1
-dp[0][0][1] = 1
-for i in range(n - 1):
+dp[1][0][0] = 1
+dp[1][1][0] = 1
+dp[1][0][1] = 1
+for i in range(1, n):
     for j in range(3):
         for k in range(3):
             dp[i + 1][j + 1][0] = (dp[i + 1][j + 1][0] + dp[i][j][k]) % MOD
@@ -14,5 +14,5 @@ for i in range(n - 1):
 ans = 0
 for i in range(3):
     for j in range(3):
-        ans = (ans + dp[n - 1][i][j]) % MOD
+        ans = (ans + dp[n][i][j]) % MOD
 print(ans)
