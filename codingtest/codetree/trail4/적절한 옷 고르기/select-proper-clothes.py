dfd106e7ae268c130a8c3498714e840546be4eb2
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2026-07-16 13:51:36 +0900

    [Passed] 적절한 옷 고르기 (196ms, 21MB)

--- codingtest/codetree/trail4/적절한 옷 고르기/select-proper-clothes.py
+++ codingtest/codetree/trail4/적절한 옷 고르기/select-proper-clothes.py
@@ -1,14 +1,14 @@
 MIN = -float('inf')
 n, m = map(int, input().split())
 arr = [tuple(map(int, input().split())) for _ in range(n)]
-dp = [[MIN] * (m + 1) for _ in range(n)]
-for i in range(n): 
-    if arr[i][0] == 1: dp[i][1] = 0
+dp = [[MIN] * n for _ in range(m + 1)]
+for i in range(n):
+    if arr[i][0] == 1: dp[1][i] = 0
 for i in range(2, m + 1):
     for j in range(n):
         s, e, v = arr[j]
         if i > e or i < s: continue
         for k in range(n):
-            if dp[k][i - 1] == MIN: continue
-            dp[j][i] = max(dp[j][i], dp[k][i - 1] + abs(v - arr[k][-1]))
-print(max([x[-1] for x in dp]))
+            if dp[i - 1][k] == MIN: continue
+            dp[i][j] = max(dp[i][j], dp[i - 1][k] + abs(v - arr[k][-1]))
+print(max(dp[-1]))
