173d90291f42b06a0614f487704f322fe2218e40
Yu Jeong SON <95585314+yuj1818@users.noreply.github.com>
2026-07-25 19:51:13 +0900

    [Passed] 최장 공통 부분 수열 (51ms, 16MB)

--- codingtest/codetree/trail4/최장 공통 부분 수열/longest-common-sequence.py
+++ codingtest/codetree/trail4/최장 공통 부분 수열/longest-common-sequence.py
@@ -9,6 +9,4 @@ for i in range(1, n + 1):
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        if dp[i][j] > ans:
-            ans = dp[i][j]
-print(ans)
+print(dp[n][m])
